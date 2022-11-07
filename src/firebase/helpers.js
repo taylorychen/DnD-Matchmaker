0116c3b441f7e4b96e1ba1703a57bb712136443b
Taylor Chen <tchen073@gmail.com>
2022-11-06 18:48:15 -0800

    allow postings to be dynamically displayed convert to mui components add helper functions to modify posts in the db

--- src/firebase/helpers.js
+++ src/firebase/helpers.js
@@ -1,14 +1,16 @@
-import { db } from "config";
+import { db } from "./config";
 import {
   doc,
   setDoc,
   getDoc,
   updateDoc,
+  deleteDoc,
   Timestamp,
   arrayUnion,
+  arrayRemove,
 } from "firebase/firestore";
 
-async function getUser(email) {
+export async function getUser(email) {
   const user = await getDoc(doc(db, "/Users/" + email));
   if (user.exists()) {
     return user.data();
@@ -16,11 +18,12 @@ async function getUser(email) {
   return null;
 }
 
-async function createUser(email, firstName, lastName, discordTag) {
+export async function createUser(email, firstName, lastName, discordTag) {
   const userRef = doc(db, "/Users/" + email);
   const userSnap = await getDoc(userRef);
   if (userSnap.exists()) {
-    return;
+    console.log(`user ${email} already exists`);
+    return false;
   }
   setDoc(userRef, {
     email: email,
@@ -28,25 +31,34 @@ async function createUser(email, firstName, lastName, discordTag) {
     lastName: lastName,
     discordTag: discordTag,
     activePostings: [],
-    archivedPostings: [],
+    inactivePostings: [],
     approvedRequests: [],
     pendingRequests: [],
   });
+  console.log(`CREATED user ${email}`);
+  return true;
 }
 
-async function createPost(email, title, desc, tags, location, maxPlayers) {
-  const userRef = doc(db, "/Users/" + email);
-  const userSnap = await getDoc(userRef);
+export async function createPost(
+  email,
+  title,
+  description,
+  tags,
+  location,
+  maxPlayers
+) {
+  const ownerRef = doc(db, "Users", email);
+  const userSnap = await getDoc(ownerRef);
   if (!userSnap.exists()) {
-    return;
+    console.log(`user ${email} not found`);
+    return false;
   }
-
   const now = Timestamp.now();
-  const postID = email + "_" + now.valueOf();
-  setDoc(doc(db, "Posts", postID), {
+  const postId = `${email}_${now.seconds}.${now.nanoseconds}`;
+  setDoc(doc(db, "Posts", postId), {
     owner: email,
     title: title,
-    description: desc,
+    description: description,
     tags: tags,
     date: now,
     location: location,
@@ -56,8 +68,110 @@ async function createPost(email, title, desc, tags, location, maxPlayers) {
     approvedUsers: [],
     pendingUsers: [],
   }).then(() => {
-    updateDoc(userRef, {
-      activePostings: arrayUnion(postID),
+    updateDoc(ownerRef, {
+      activePostings: arrayUnion(postId),
     });
   });
+  console.log(`CREATED post for ${email}`);
+  return true;
 }
+
+/**
+ * Delete post with specified postId
+ * @param {string} postId
+ * @param {string} owner
+ */
+export async function deletePost(postId, owner) {
+  if (!validPostOwner(postId, owner)) {
+    console.log("invalid post owner");
+    return false;
+  }
+
+  // get refs to documents
+  const ownerRef = doc(db, "Users", owner);
+  const postRef = doc(db, "Posts", postId);
+  deleteDoc(postRef).then(() => {
+    updateDoc(ownerRef, {
+      activePostings: arrayRemove(postId),
+    });
+  });
+  console.log(`DELETED post ${postId}`);
+  return true;
+}
+
+/**
+ * set post to active
+ * @param {string} postId
+ * @param {string} owner
+ */
+export async function setActive(postId, owner) {
+  if (!validPostOwner(postId, owner)) {
+    console.log("invalid post owner");
+    return false;
+  }
+
+  // get refs to documents
+  const ownerRef = doc(db, "Users", owner);
+  const postRef = doc(db, "Posts", postId);
+
+  updateDoc(postRef, {
+    active: true,
+  })
+    // add to active postings
+    .then(() => {
+      updateDoc(ownerRef, {
+        activePostings: arrayUnion(postId),
+      });
+    })
+    // remove from inactive postings
+    .then(() => {
+      updateDoc(ownerRef, {
+        inactivePostings: arrayRemove(postId),
+      });
+    });
+  console.log(`set post ${postId} to ACTIVE`);
+  return true;
+}
+
+/**
+ * set post to inactive
+ * @param {string} postId
+ * @param {string} owner
+ */
+export async function setInactive(postId, owner) {
+  if (!validPostOwner(postId, owner)) {
+    console.log("invalid post owner");
+    return false;
+  }
+
+  // get refs to documents
+  const ownerRef = doc(db, "Users", owner);
+  const postRef = doc(db, "Posts", postId);
+  updateDoc(postRef, {
+    active: false,
+  })
+    // add to inactive postings
+    .then(() => {
+      updateDoc(ownerRef, {
+        inactivePostings: arrayUnion(postId),
+      });
+    })
+    // remove from active postings
+    .then(() => {
+      updateDoc(ownerRef, {
+        activePostings: arrayRemove(postId),
+      });
+    });
+  console.log(`set post ${postId} to INACTIVE`);
+  return true;
+}
+
+/**
+ * verifies that the postId matches the owner
+ * @param {string} postId
+ * @param {string} owner
+ */
+const validPostOwner = (postId, owner) => {
+  if (owner.length >= postId.length) return false;
+  return postId.substring(0, owner.length) === owner;
+};
