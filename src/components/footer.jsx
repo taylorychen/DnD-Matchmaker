e61cc78e42cad460f8989a8b97b20281dc8c5d97
Annie Tran <88398285+abt14@users.noreply.github.com>
2022-11-14 11:17:55 -0800

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,23 +1,33 @@
-import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./footer.css";
 
-function Footer() {
-  return (
-    <nav>
-      <ul>
-      <div>Footer</div>
-        <li>
-          <Link to="/Postings">Postings</Link>
-        </li>
-        <li>
-          <Link to="/Profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/haha">Error</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <h3>Unhinged: Dungeons & Dragons Matchmaker</h3>
+        <p>
+          Unhinged: D&D Matchmaker is a web application that creates a fun and
+          easy way for D&D players to find other players to play with!
+        </p>
+        <p>
+          Developed By: Taylor Chen | Connor Choi | Gayatri Puppala | Annie Tran
+          | Angeline Xu
+        </p>
+        <div className="footer-links">
+          <ul>
+            <li>
+              <a href="/Profile">Home</a>
+            </li>
+            <li>
+              <a href="/Postings">Postings</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer;
+export default Footer;
