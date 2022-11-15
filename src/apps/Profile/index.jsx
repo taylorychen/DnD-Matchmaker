0643896a7453d8352548ba8924a895b7affa8d93
Annie Tran <88398285+abt14@users.noreply.github.com>
2022-11-14 21:05:12 -0800

    fetch & display profile info

--- src/apps/Profile/index.jsx
+++ src/apps/Profile/index.jsx
@@ -1,41 +1,57 @@
 import React, { useState } from "react";
-import Header from "../../components/header";
-import Footer from "../../components/footer";
+import {
+  getCurrentUserName,
+  getCurrentUserDiscord,
+  updateCurrentUserDiscord,
+  updateCurrentUserName,
+} from "../../firebase/helpers";
 import "./profile.css";
+import { currentUserEmail } from "../../firebase/auth";
 
 export default function Profile() {
   const [name, setName] = useState("Your Name");
-  const [username, setUsername] = useState("Your Username");
-  const [email, setEmail] = useState("Your email");
   const [discord, setDiscord] = useState("Your discord");
   const [active, setActive] = useState("edit");
+
+  getCurrentUserName().then((response) => {
+    setName(response);
+  });
+  getCurrentUserDiscord().then((response) => {
+    if (response === null) {
+      setDiscord("Your discord");
+    } else {
+      setDiscord(response);
+    }
+  });
 
   //Edit Section of the Profile
   function Edit() {
     return (
       <div className="edit">
         <h3>EDIT PROFILE</h3>
-        <div className="edit-parent">
-          <div className="edit-item">
-            <label>
-              <p>Name</p>
-            </label>
-            <input type="text" name="name" placeholder={name} />
+        <form id="update">
+          <div className="edit-parent">
+            <div className="edit-item">
+              <label>
+                <p>Name</p>
+              </label>
+              <input type="text" name="name" placeholder={name} />
+            </div>
+            <div className="edit-item">
+              <label>Discord</label>
+              <input type="text" name="discord" placeholder={discord} />
+            </div>
+            <button
+              form="update"
+              onClick={() => {
+                updateCurrentUserDiscord(discord);
+                updateCurrentUserName(name);
+              }}
+            >
+              Update
+            </button>
           </div>
-          <div className="edit-item">
-            <label>Username</label>
-            <input type="text" name="username" placeholder={username} />
-          </div>
-          <div className="edit-item">
-            <label>Email</label>
-            <input type="text" name="email" placeholder={email} />
-          </div>
-          <div className="edit-item">
-            <label>Discord</label>
-            <input type="text" name="discord" placeholder={discord} />
-          </div>
-          <button>Update</button>
-        </div>
+        </form>
       </div>
     );
   }
@@ -59,17 +75,17 @@ export default function Profile() {
   }
 
   return (
-    <div>
+    <div className="profile-container">
       <h1>My Profile</h1>
       <div className="container">
         <div className="sub-container">
           <div className="filler"></div>
           <h3>{name}</h3>
           <div className="profile-info">
-            <p className="info-tag">Username:</p>
-            <p className="info">{username}</p>
+            {/* <p className="info-tag">Username:</p>
+            <p className="info">{username}</p> */}
             <p className="info-tag">Email:</p>
-            <p className="info">{email}</p>
+            <p className="info">{currentUserEmail()}</p>
             <p className="info-tag">Discord:</p>
             <p className="info">{discord}</p>
           </div>
