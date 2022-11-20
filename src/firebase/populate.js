2f5cb10bd8ff6143bd45608716e3950de02b2173
Taylor Chen <tchen073@gmail.com>
2022-11-19 19:11:40 -0800

    edit populate to work with tags

--- src/firebase/populate.js
+++ src/firebase/populate.js
@@ -2,12 +2,31 @@ import { faker } from "@faker-js/faker";
 import { createPost } from "./helpers";
 
 const users = [
-    "taylor@gmail.com",
+    "tchen073@gmail.com",
     "cmchoi220@gmail.com",
-    "annie@gmail.com",
-    "angeline@gmail.com",
+    "annietran04@g.ucla.edu",
+    "agx191@g.ucla.edu",
     "gpuppala@g.ucla.edu",
 ];
+
+const locations = [
+    "Wooden Games Lounge",
+    "Sunset Rec",
+    "Olympic Hall Lounge",
+    "Reiber Court Lounge",
+    "Sproul Hall Lounge",
+    "Covel Commons",
+    "Royce Hall 154",
+    "Janss Steps",
+    "123 Kelton",
+    "4567 Strathmore",
+    "ACM Clubhouse",
+    "Ackerman Bruin Fun Zone",
+];
+
+const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 /**
  * creates random posts in the database
@@ -15,16 +34,30 @@ const users = [
  */
 export function createRandomPosts(number) {
     for (let i = 0; i < number; i++) {
-        const randomNum = Math.floor(Math.random() * users.length);
-        const owner = users[randomNum];
-        const title = `${faker.word.adjective()} game`;
+        const owner = users[Math.floor(Math.random() * users.length)];
+        const title = `A ${capitalize(faker.word.adjective())} Game`;
         const description = faker.lorem.sentences();
-        const location = faker.address.cityName();
-        const maxPlayers = Math.floor(Math.random() * 15) + 1;
-        const tags = [];
-        for (let j = 0; j < Math.floor(Math.random() * 5); j++) {
-            tags.push(faker.word.adjective());
-        }
-        createPost(owner, title, description, tags, location, maxPlayers);
+        const location =
+            locations[Math.floor(Math.random() * locations.length)];
+        const maxPlayers = Math.floor(Math.random() * 14) + 2;
+
+        // tags
+        const t_campaign = Math.random() < 0.5;
+        const t_homebrew = Math.random() < 0.5;
+        const t_looseRules = Math.random() < 0.5;
+        const t_oneShot = Math.random() < 0.5;
+        const t_preWritten = Math.random() < 0.5;
+        createPost(
+            owner,
+            title,
+            description,
+            t_looseRules,
+            t_oneShot,
+            t_campaign,
+            t_homebrew,
+            t_preWritten,
+            location,
+            maxPlayers
+        );
     }
 }
